46d9eef8d209107d4144b103583addeaba6f6c16
Russell Barnes <russell@russellbarnes.co.uk>
2013-10-14 14:33:51 +0100

    Update nanpy_avoider.py

--- nanpy_avoider.py
+++ nanpy_avoider.py
@@ -13,7 +13,7 @@ Arduino.pinMode(lock, Arduino.INPUT)
 
 def debug():
 """
-If lock switch is in the locked position stop motors and give sensor feedback
+If lock switch is in the locked position stop motors and give sensor feedback instead
 """
     stop()
     reading = read_sensors()
@@ -37,18 +37,29 @@ def main():
             if locked == 0:
                 debug()
             else:
+                ### Actual Avoider coder starts below ###
                 if not front_bump():
-                    go(100)
+                    if not left_bump() and not right_bump():
+                        print "No bump detected - on we go!"
+                        go(200)
+                        if left_bump() and not right_bump():
+                            print "Left bump detected... Turning right"
+                            rot_cw(100, 200)
+                        elif right_bump() and not left_bump():
+                            print "Right bump detected... Turning left"
+                            rot_ccw(100, 200)
+                        elif left_bump() and right_bump():
+                            print "Both left & right bumps detected... Turning round!"
+                            rot_cw(500, 200)
                 else:
-                    ### Avoider coder starts below ###
-                    stop()
-                    reading = read_sensors()
-                    print reading
-                    ### Avoider code ends above ###
+                    print "Front bump detected... Turning round!"
+                    rot_cw(500, 200)
+                # avoider code ends above
         except KeyboardInterrupt:
             close_pins()
             print "\nExiting"
             quit()
+
 
 if __name__ == '__main__':
     main()
