af9e89002366f8ecddc7456e1fe12350f1e7464a
root <root@mypi.(none)>
2013-10-17 00:04:55 +0100

    tweaked helperfuncs

--- RDuD2/modules/helperfuncs.py
+++ RDuD2/modules/helperfuncs.py
@@ -6,6 +6,8 @@
 #  Copyright 2013 Russell Barnes
 #  
 
+from nanpy import Arduino
+
 def map(x, in_min, in_max, out_min, out_max):
     """
     Arduino-style map function to take input min/max and apply to 
@@ -20,25 +22,17 @@ def degrees(x): ### Tweak specifically for you own robot with testing ###
     """
     return (x - 0) * (1021 - 0) / (359 - 0) + 0
 
-def pan_angle(x)
+def pan_angle(x):
     """
     Pan servo range should be constrain between 0-149
     (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
     """
     return (x - 0) * (149 - 0) / (179 - 0) + 0
 
-def tilt_angle(x)
+def tilt_angle(x):
     """
     Tilt servo range should be constrained between 50-179
     (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
     """
     return (x - 0) * (179 - 50) / (179 - 0) + 50
 
-def close_pins():
-    """
-    Close all motor pins to quit cleanly upon end or exception
-    """
-    Arduino.analogWrite(speedA, Arduino.LOW)
-    Arduino.analogWrite(speedB, Arduino.LOW)
-    Arduino.digitalWrite(dirA, Arduino.LOW)
-    Arduino.digitalWrite(dirB,Arduino.LOW)
